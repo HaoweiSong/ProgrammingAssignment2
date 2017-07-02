ca101f42e326a48dea745150bc9b30f2dc460200
Haowei Song <songhw@gmail.com>
2017-07-02 11:29:56 -0500

    function changed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+#<<- superassignment operator, something like the global variables in C/C++
+## creates a special "matrix" object that can cache its inverse
 
-## Write a short comment describing this function
+## Generate a vector of fucntions to do:
+## set(matrix), set the value of the original matrix, also use to initialize the fucntion
+## get(), get  the value of the matrix
+## setinverse(matrix) the value of the inversed matrix
+## getinverse(), get the value of the inversed matrix
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    #inversed_x is the cache of inversed matrix of x
+    inversed_x <- NULL
+    set <- function(y) {
+        x <<- y
+        inversed_x <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inversed) inversed_x <<- inversed
+    getinverse <- function() inversed_x
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+#Test if the matrix is inveresed or not. If it is return it, it is not inverse and cache it
+#the argument x is the vector of functions that generated from "makeCacheMatrix"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inversed_x <- x$getinverse()
+    if(!is.null(inversed_x)) {
+        message("getting inversed matrix")
+        return(inversed_x)
+    }
+    data <- x$get()
+    inversed_x <- solve(data, ...)
+    x$setinverse(inversed_x)
+    inversed_x
 }
